45493a2aa523a9150435db5584bf249d50f67500
wazn <sharef050523@icloud.com>
2026-08-06 19:45:48 +0300

    Add functions to fetch and update Salla products

--- sallaService.js
+++ sallaService.js
@@ -1,1 +1,34 @@
+const axios = require('axios');
+const db = require('./db');
+
+async function getSallaToken() {
+    const res = await db.query('SELECT access_token FROM store_settings LIMIT 1');
+    return res.rows[0]?.access_token;
+}
+
+async function fetchSallaProducts() {
+    const token = await getSallaToken();
+    if (!token) throw new Error("لا يوجد Access Token لمنصة سلة");
+
+    const response = await axios.get('https://api.salla.dev/admin/v2/products', {
+        headers: { 'Authorization': `Bearer ${token}` }
+    });
+    return response.data.data;
+}
+
+async function updateSallaProductPrice(sallaProductId, newPrice) {
+    const token = await getSallaToken();
+    if (!token) throw new Error("لا يوجد Access Token لمنصة سلة");
+
+    await axios.put(`https://api.salla.dev/admin/v2/products/${sallaProductId}`, {
+        price: newPrice
+    }, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+module.exports = { fetchSallaProducts, updateSallaProductPrice };
 
